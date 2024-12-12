2b954bc195348b211826f1b726f97b7060d6148a
“sangmiiiiiin” <kwk680037@gmail.com>
2024-12-12 21:14:07 +0900

    🙈 tf 파일 생성

---
+++ lab1/provider.tf
@@ -0,0 +1,15 @@
+# Configure the AWS Provider
+terraform {
+  required_version = ">=1.0.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
